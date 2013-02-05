4ac85ccd36781000c2de108dbc122bd761b600c3
Christian Waechter <waechter@in.tum.de>
2013-02-05 10:38:49 +0100

    added header for generating some uniform/normal random numbers modified test to integrate the distribution functors

---
+++ src/utMath/Functors/UniformDistribution.h
@@ -0,0 +1,123 @@
+/*
+ * Ubitrack - Library for Ubiquitous Tracking
+ * Copyright 2006, Technische Universitaet Muenchen, and individual
+ * contributors as indicated by the @authors tag. See the
+ * copyright.txt in the distribution for a full listing of individual
+ * contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+// TODO: implement module start/stop mechanics
+
+/**
+ * @ingroup math
+ * @file
+ * Implements a functor for generating uniformly distributed vectors.
+ * Idealy this functor is combined with std::generate_n
+ *
+ * @author Christian Waechter <christian.waechter@in.tum.de>
+ */
+
+#ifndef __UBITRACK_UNIFORM_DISTRIUBUTION_H_INCLUDED__
+#define __UBITRACK_UNIFORM_DISTRIUBUTION_H_INCLUDED__ 
+
+// std
+#include <iterator>
+#include <functional>
+
+// boost
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/math/constants/constants.hpp>
+
+// Ubitrack
+#include <utMath/Vector.h>
+#include <utMath/Quaternion.h>
+#include "../RandomNumbers.h"
+
+
+namespace Ubitrack { namespace Math { namespace Functors {
+
+/**
+ * Function that generates a vector of uniformly distributed random numbers.
+ * Depending on the constructor call you have two options to define the limits.
+ * Either you specify the limits of thr rango to sample from for each dimension
+ * or you specify one limit that
+ */
+template< std::size_t N, typename T >
+struct uniform_distribution
+	: public std::unary_function< void, Math::Vector< N, T > >
+{
+	protected:
+		const Math::Vector< N, T > m_min_range;
+		const Math::Vector< N, T > m_max_range;
+		
+	public :
+		uniform_distribution( const T min_range , const T max_range )
+			: std::unary_function< void, Math::Vector< N, T > >( )
+			, m_min_range( boost::numeric::ublas::scalar_vector< T >( N, std::min( min_range, max_range ) ) )
+			, m_max_range( boost::numeric::ublas::scalar_vector< T >( N, std::max( min_range, max_range ) ) )
+			{ };
+			
+		uniform_distribution( const Math::Vector< N, T > &min_range, const Math::Vector< N, T > &max_range )
+			: std::unary_function< void, Math::Vector< N, T > >( )
+			, m_min_range( min_range )
+			, m_max_range( max_range )
+			{ };
+
+		const Math::Vector< N, T > operator()( void ) const
+		{
+			Math::Vector< N, T > vec; 
+			for( std::size_t n( 0 ); n < N; ++n )
+				vec( n ) = Math::distribute_uniform< T >( m_min_range( n ), m_max_range( n ) );
+			return vec;
+		}
+};
+
+
+/**
+ * Function that generates a uniformly distributed quaternion.
+ * the functions follows the explanation regarding random unit quaternions from the following webside:
+ * http://planning.cs.uiuc.edu/node198.html
+ */
+template< typename T = double >
+struct uniform_quaternion
+	: public std::unary_function< void, Math::Quaternion >
+{
+
+	public :
+		uniform_quaternion( )
+			: std::unary_function< void, Math::Quaternion >( )
+			{ };
+	
+		const Math::Quaternion operator()( void ) const
+		{
+			const T x = Math::distribute_uniform< T >( 0, 1 );
+			const T y = Math::distribute_uniform< T >( 0, 1 );
+			const T z = Math::distribute_uniform< T >( 0, 1 );
+			
+			const T rootx = std::sqrt( x );
+			const T rootxinv = std::sqrt( 1 - x );
+			const T piz2 = 2 * boost::math::constants::pi< T >() * z;
+			const T piy2 = 2 * boost::math::constants::pi< T >() * y;
+			
+			return Math::Quaternion( rootxinv * std::sin( piy2 ), rootxinv * std::cos( piy2 ), rootx * std::sin( piz2 ), rootx * std::cos( piz2 ) );
+		}
+};
+
+} } } //Ubitrack::Math::Functors
+
+#endif // __UBITRACK_UNIFORM_DISTRIUBUTION_H_INCLUDED__
--- tests/Calibration/2D3DPoseEstimationTest.cpp
+++ tests/Calibration/2D3DPoseEstimationTest.cpp
@@ -4,38 +4,50 @@
 #include <utCalibration/2D3DPoseEstimation.h>
 #include "../tools.h"
 #include <boost/numeric/ublas/vector_proxy.hpp>
+#include <utMath/RandomNumbers.h>
+#include <utMath/Functors/UniformDistribution.h>
 
 #include <iostream>
+#include <algorithm>
 
 using namespace Ubitrack::Math;
 namespace ublas = boost::numeric::ublas;
 
-void TestOptimizePose()
+template< typename T >
+void TestOptimizePose( const std::size_t n_runs, const T epsilon )
 {
-	for ( int iRun = 0; iRun < 10; iRun++ )
+
+	Functors::uniform_quaternion<> randQuat;
+	Functors::uniform_distribution< 3, T > randVector( -0.4, 0.4 );
+	Functors::uniform_distribution< 3, T > randTranslation( -100, 100 );
+	
+	for ( int iRun = 0; iRun < n_runs; iRun++ )
 	{
 		// random intrinsics matrix
-		Matrix< 3, 3, double > cam( ublas::identity_matrix< double >( 3 ) );
-		cam( 0, 0 ) = random( 200, 800 );
-		cam( 1, 1 ) = random( 200, 800 );
+		Matrix< 3, 3, T > cam( ublas::identity_matrix< T >( 3 ) );
+		cam( 0, 0 ) = distribute_uniform< T >( 200, 800 );
+		cam( 1, 1 ) = distribute_uniform< T >( 200, 800 );
 		
 		// random pose
-		Quaternion rot( randomQuaternion() );
-		Vector< 3, double > trans( randomVector< 3, double >() );
-		trans( 2 ) = random( 1.0, 2.0 );
+		Quaternion rot( randQuat( ) );
+		Vector< 3, T > trans ( randTranslation() );
+		trans( 2 ) = distribute_uniform< T >( 1, 10 );
 		
 		// some random 3d points
-		std::vector< Vector< 3, double > > p3D( 15 );
-		for ( unsigned i = 0; i < p3D.size(); i++ )
-			p3D[ i ] = randomVector< 3 >( 0.4 );
+		const std::size_t n( distribute_uniform< std::size_t >( 5, 30 ) );
+		std::vector< Ubitrack::Math::Vector< 3, T > > p3D;
+		p3D.reserve( n );
+		std::generate_n ( std::back_inserter( p3D ), n,  randVector );
+		// std::copy( p3D.begin(), p3D.end(), std::ostream_iterator< Ubitrack::Math::Vector< 3 > > ( std::cout, ", ") );
 			
 		// project to 2D points 
-		std::vector< Vector< 2, double > > p2D( p3D.size() );
-		for ( unsigned i = 0; i < p3D.size(); i++ )
+		std::vector< Vector< 2, T > > p2D;
+		p2D.reserve( n );
+		for ( std::size_t i = 0; i < n; i++ )
 		{
-			Vector< 3, double > tmp( rot * p3D[ i ] + trans );
+			Vector< 3, T > tmp( rot * p3D[ i ] + trans );
 			tmp = ublas::prod( cam, tmp );
-			p2D[ i ] = ublas::subrange( tmp, 0, 2 ) / tmp( 2 );
+			p2D.push_back( ublas::subrange( tmp, 0, 2 ) / tmp( 2 ) );
 		}
 		
 		// add some noise to the pose
@@ -45,20 +57,20 @@ void TestOptimizePose()
 			trans + randomVector< 3 >( 0.2 ) );
 			
 		Pose optimized( testPose );
-			
+		
 		// run optimizePose
 		Ubitrack::Calibration::optimizePose( optimized, p2D, p3D, cam );
 		
 		// check if pose is better than before
 		BOOST_CHECK( quaternionDiff( testPose.rotation(), rot ) >= quaternionDiff( optimized.rotation(), rot ) );
 		BOOST_CHECK( ublas::norm_2( testPose.translation() - trans ) >= ublas::norm_2( optimized.translation() - trans ) );
-		BOOST_CHECK_SMALL( quaternionDiff( optimized.rotation(), rot ), 1e-3 );
-		BOOST_CHECK_SMALL( ublas::norm_2( optimized.translation() - trans ), 1e-3 );
+		BOOST_CHECK_SMALL( quaternionDiff( optimized.rotation(), rot ), epsilon );
+		BOOST_CHECK_SMALL( ublas::norm_2( optimized.translation() - trans ), epsilon );
 	}
 }
 
 void Test2D3DPoseEstimation()
 {
-	TestOptimizePose();
+	TestOptimizePose< double >( 10, 1e-3 );
 }
 
