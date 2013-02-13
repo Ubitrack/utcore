8873c3620e2ea5ae22d10c8442a9980f7be502de
Frieder Pankratz <pankratz@in.tum.de>
2013-02-13 15:36:59 +0100

    linux windows filesystem problems

--- src/utUtil/GlobFiles.cpp
+++ src/utUtil/GlobFiles.cpp
@@ -48,7 +48,7 @@ void globFiles( const std::string& directory, const std::string & patternString,
 			boost::filesystem::path p( *it );
 #endif
 			// check for files with suitable extension
-#ifdef BOOST_FILESYSTEM_I18N
+#ifdef WIN32
 			if ( boost::filesystem::exists( p ) && ! boost::filesystem::is_directory( p ) && boost::regex_match( p.filename().string(), ext ) )
 #else
 			if ( boost::filesystem::exists( p ) && ! boost::filesystem::is_directory( p ) && boost::regex_match( p.filename(), ext ) )
