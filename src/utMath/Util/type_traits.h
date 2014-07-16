66fe9b2b9d77ffbd4e96ac6dc68595ec8f470719
Christian Waechter <christian.waechter@in.tum.de>
2014-07-16 12:50:44 +0200

    added some more traits to determine the precision of a measurement type at compile time

--- src/utMath/Util/type_traits.h
+++ src/utMath/Util/type_traits.h
@@ -12,7 +12,7 @@ namespace Ubitrack { namespace Math { namespace Util {
  *
  * This functor reassembles the integral constant struct,
  * similar to the c++0x standard and supports the writing
- * of compile time dependend code.
+ * of compile time dependent code.
  * see also http://en.cppreference.com/w/cpp/types/integral_constant
  */
 template< class T, T v >
@@ -45,6 +45,45 @@ struct has_dynamic_storage
 struct fixed_storage_tag {};
 struct dynamic_storage_tag {};
 struct unknown_storage_tag {};
+
+
+/** 
+ * type_trait that defines the precision of a datatypes' underlying built-in type
+ *
+ * as a default it tries to determine the precision from a defined typedef.
+ *
+ * it is overwritten for the following built-in-types:
+ *   - \c float
+ *   - \c double
+ *   - \c long \c double
+ * where it defaults to the built-in type itself
+ */
+template< typename T >
+struct precision
+{
+	typedef typename T::value_type type;
+};
+
+/// @internal specialization for type float
+template<>
+struct precision< float >
+{
+	typedef float type;
+};
+
+/// @internal specialization for type double
+template<>
+struct precision< double >
+{
+	typedef double type;
+};
+
+/// @internal specialization for type long double
+template<>
+struct precision< long double >
+{
+	typedef long double type;
+};
 
 } } } // namespace Ubitrack::Math::Util
 
