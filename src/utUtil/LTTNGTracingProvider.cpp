20243fc85b173648de5fbfabfce8e71ceb81f995
Ulrich Eck <ulricheck@gmail.com>
2016-07-14 17:55:03 +0200

    still trying to fix LTTng TracePoint provider linking ... found a cmake based example in the documentation ..

--- src/utUtil/LTTNGTracingProvider.cpp
+++ src/utUtil/LTTNGTracingProvider.cpp
@@ -4,9 +4,8 @@
 
 #ifdef HAVE_LTTNGUST
 
-//#define TRACEPOINT_CREATE_PROBES
+#define TRACEPOINT_CREATE_PROBES
 #define TRACEPOINT_DEFINE
-#define TRACEPOINT_PROBE_DYNAMIC_LINKAGE
 
 #include "utUtil/LTTNGTracingProvider.h"
 
