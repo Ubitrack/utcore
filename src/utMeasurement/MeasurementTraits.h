5ea66fd02f4b2929f0383d356e59add71e275fc5
Ulrich Eck <ulrich.eck@tum.de>
2019-08-10 16:26:15 +0200

    fixed printing of measurement-types

--- src/utMeasurement/MeasurementTraits.h
+++ src/utMeasurement/MeasurementTraits.h
@@ -40,70 +40,7 @@ namespace Ubitrack {
                 Image // Forward declaration as we cannot extend an enumeration later
             };
 
-            std::ostream& operator<<( std::ostream& s, const MeasurementType& m )
-            {
-                switch (m) {
-                    case MeasurementType::Undefined:
-                        s << "Undefined";
-                        break;
-                    case MeasurementType::ScalarInt:
-                        s << "ScalarInt";
-                        break;
-                    case MeasurementType::ScalarDouble:
-                        s << "ScalarDouble";
-                        break;
-                    case MeasurementType::ScalarUnsignedLong:
-                        s << "ScalarUnsignedLong";
-                        break;
-                    case MeasurementType::Vector2:
-                        s << "Vector2";
-                        break;
-                    case MeasurementType::Vector3:
-                        s << "Vector3";
-                        break;
-                    case MeasurementType::Vector4:
-                        s << "Vector4";
-                        break;
-                    case MeasurementType::Vector8:
-                        s << "Vector8";
-                        break;
-                    case MeasurementType::Quaternion:
-                        s << "Quaternion";
-                        break;
-                    case MeasurementType::Matrix3x3:
-                        s << "Matrix3x3";
-                        break;
-                    case MeasurementType::Matrix3x4:
-                        s << "Matrix3x4";
-                        break;
-                    case MeasurementType::Matrix4x4:
-                        s << "Matrix4x4";
-                        break;
-                    case MeasurementType::Pose:
-                        s << "Pose";
-                        break;
-                    case MeasurementType::ErrorPose:
-                        s << "ErrorPose";
-                        break;
-                    case MeasurementType::ErrorVector2:
-                        s << "ErrorVector2";
-                        break;
-                    case MeasurementType::ErrorVector3:
-                        s << "ErrorVector3";
-                        break;
-                    case MeasurementType::RotationVelocity:
-                        s << "RotationVelocity";
-                        break;
-                    case MeasurementType::CameraIntrinsics:
-                        s << "CameraIntrinsics";
-                        break;
-                    case MeasurementType::Image:
-                        s << "Image";
-                        break;
-                }
-                return s;
-            }
-
+            std::ostream& operator<<( std::ostream& s, const MeasurementType& m );
 
             template< typename T >
             struct MeasurementTypeToEnumTraits
